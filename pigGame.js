30422e2fbfd96baef4c4b36d64430e43eea4802b
Haim <hgq_2000@hotmail.com>
2022-12-03 23:03:21 +0000

    Import class Player

--- pigGame.js
+++ pigGame.js
@@ -1,45 +1,4 @@
-class Player{
-    constructor(player){
-        this.player = player;
-        this.points = parseInt(document.querySelector(`#${player}__points`).innerHTML)
-        this.currentPoints = parseInt(document.querySelector(`#${player}__currentPoints`).innerHTML)
-        this.bg = document.querySelector(`.${player}`);
-        this.winner = false;
-        this.turn = false;
-    }
-
-    getPoints() {
-        return this.points;
-    }
-
-    getCurrentPoints(){
-        return this.currentPoints;
-    }
-
-    changeTurn(){
-        this.turn = !this.turn;
-        if( this.turn ){
-            this.bg.classList.add("player--active");
-        }else{
-            this.bg.classList.remove("player--active");
-        }
-    }
-
-    setPoints(__points){
-        this.points += __points;
-        document.querySelector(`#${this.player}__points`).innerHTML = this.points;
-    }
-
-    setCurrentPoints(__currentPoints){
-        this.currentPoints = this.currentPoints + __currentPoints;
-        document.querySelector(`#${this.player}__currentPoints`).innerHTML = this.currentPoints;
-    }
-
-    resetCurrentPoints(){
-        this.currentPoints = 0;
-        document.querySelector(`#${this.player}__currentPoints`).innerHTML = this.currentPoints;
-    }
-}
+import Player from './Player.js'
 
 class Dice{
     constructor(dice){
